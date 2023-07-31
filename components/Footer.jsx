502c611a34d510ac777be739d7aa7aa2fd96c521
FoPPi <mishk908@gmail.com>
2023-07-31 22:27:09 +0300

    0.0.5

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="px-5 mt-auto">
             <ShowWithDelay delay={1.4}>
             <div className={`flex flex-col sm:flex-row mt-3 mb-5 items-center justify-center space-x-5 sm:space-x-24 lg:space-x-48`}>
-                <a className="text-[#838383] hover:text-[#822FDF] transform duration-300" href="https://green-line-studio.vercel.app/" target="_blank">Developed by: Green Line Studio</a>
+                <a className="text-[#838383] hover:text-[#822FDF] transform duration-300" href="https://green-line-studio.xyz/" target="_blank">Developed by: Green Line Studio</a>
                 <p className="text-[#838383]">©2023 - Chaibot</p>
             </div>
             </ShowWithDelay>
